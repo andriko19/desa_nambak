b7db9b0724a6ebdec09ea260783386b2b6c46b46
Andrik <andrik.suprayitno@gmail.coom>
2023-08-29 23:11:38 +0700

    modified topbar

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,7 +7,19 @@
     {{-- <link href="{{ URL::asset('/assets/libs/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css" /> --}}
 @endsection
 
-
+  @section('section-index')
+    <section id="hero-fullscreen" class="hero-fullscreen d-flex align-items-center">
+      <div class="container d-flex flex-column align-items-center position-relative" data-aos="zoom-out">
+        <h2>Welcome to <span>HeroBiz</span></h2>
+        <p>Et voluptate esse accusantium accusamus natus reiciendis quidem voluptates similique aut.</p>
+        <div class="d-flex">
+          <a href="#about" class="btn-get-started scrollto">Get Started</a>
+          <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+        </div>
+      </div>
+    </section>
+  @endsection
+  
   @section('content')
 
     <!-- ======= Featured Services Section ======= -->
@@ -53,7 +65,7 @@
       </div>
     </section><!-- End Featured Services Section -->
 
-    <!-- ======= About Section ======= -->
+    {{-- <!-- ======= About Section ======= -->
     <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
@@ -1192,7 +1204,7 @@
         </div>
 
       </div>
-    </section><!-- End Contact Section -->
+    </section><!-- End Contact Section --> --}}
 
   @endsection
 
--- resources/views/frontend/layouts/topbar.blade.php
+++ resources/views/frontend/layouts/topbar.blade.php
@@ -1,77 +1,128 @@
 <!-- ======= Header ======= -->
 <header id="header" class="header fixed-top" data-scrollto-offset="0">
-<div class="container-fluid d-flex align-items-center justify-content-between">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
 
-    <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
-    <!-- Uncomment the line below if you also wish to use an image logo -->
-    <!-- <img src="assets/img/logo.png" alt=""> -->
-    <h1>HeroBiz<span>.</span></h1>
-    </a>
+        <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Ds<span>Nambak</span></h1>
+        </a>
 
-    <nav id="navbar" class="navbar">
-    <ul>
-
-        <li class="dropdown"><a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+        <nav id="navbar" class="navbar">
         <ul>
-            <li><a href="index.html" class="active">Home 1 - index.html</a></li>
-            <li><a href="index-2.html">Home 2 - index-2.html</a></li>
-            <li><a href="index-3.html">Home 3 - index-3.html</a></li>
-            <li><a href="index-4.html">Home 4 - index-4.html</a></li>
-        </ul>
-        </li>
 
-        <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
-        <li><a class="nav-link scrollto" href="index.html#services">Services</a></li>
-        <li><a class="nav-link scrollto" href="index.html#portfolio">Portfolio</a></li>
-        <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
-        <li><a href="blog.html">Blog</a></li>
-        <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-        <ul>
-            <li>
-            <a href="#">Column 1 link 1</a>
-            <a href="#">Column 1 link 2</a>
-            <a href="#">Column 1 link 3</a>
+            <li class="dropdown"><a href="#"><span>Profil</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li><a href="#">Profil Desa</a></li>
+                </ul>
             </li>
-            <li>
-            <a href="#">Column 2 link 1</a>
-            <a href="#">Column 2 link 2</a>
-            <a href="#">Column 3 link 3</a>
+
+            <li class="dropdown"><a href="#"><span>Informasi Publik</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li class="dropdown"><a href="#"><span>Informasi Reproduksi</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                        <ul>
+                            <li><a href="#">Informasi Reproduksi Perempuan Perem </a></li>
+                            <li><a href="#">Informasi Reproduksi Laki-laki</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#"><span>Informasi Pencegahan kawin</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                        <ul>
+                            <li><a href="#">Pendidikan Seksual</a></li>
+                            <li><a href="#">Komunikasi Terbuka</a></li>
+                            <li><a href="#">Kepercayaan diri dan keterampilan pos</a></li>
+                            <li><a href="#">Membangun nilai diri yang positif</a></li>
+                            <li><a href="#">Menghindari tekanan teman sebaya</a></li>
+                            <li><a href="#">Memahami Konsekuensi dan resiko</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#"><span>Informasi Penanganan remaja kawin</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                        <ul>
+                            <li><a href="#">Pendidikan seksual</a></li>
+                            <li><a href="#">Konseling keluarga</a></li>
+                            <li><a href="#">Pemahaman tanggung jawab Perempuan</a></li>
+                            <li><a href="#">Pengemdalian Emosi</a></li>
+                            <li><a href="#">Perlindungan hukum</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </li>
-            <li>
-            <a href="#">Column 3 link 1</a>
-            <a href="#">Column 3 link 2</a>
-            <a href="#">Column 3 link 3</a>
+
+             <li class="dropdown"><a href="#"><span>Data dan Informasi</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li><a href="#">Jumlah Remaja Preventif , Jenis Kelamin dan Usia</a></li>
+                    <li><a href="#">Jumlah Remaja Dispensasi Kawin, Usia, Jenis Kelamin</a></li>
+                </ul>
             </li>
-            <li>
-            <a href="#">Column 4 link 1</a>
-            <a href="#">Column 4 link 2</a>
-            <a href="#">Column 4 link 3</a>
+
+            <li class="dropdown"><a href="#"><span>Layanan Publik</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li><a href="#">Profil Desa</a></li>
+                </ul>
             </li>
-        </ul>
-        </li>
-        <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-        <ul>
-            <li><a href="#">Drop Down 1</a></li>
-            <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+
+            <li class="dropdown"><a href="#"><span>Parenting</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li><a href="#">Profil Desa</a></li>
+                </ul>
+            </li>
+            
+
+            {{-- <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
+            <li><a class="nav-link scrollto" href="index.html#services">Services</a></li>
+            <li><a class="nav-link scrollto" href="index.html#portfolio">Portfolio</a></li>
+            <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
+            <li><a href="blog.html">Blog</a></li>
+            <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-                <li><a href="#">Deep Drop Down 1</a></li>
-                <li><a href="#">Deep Drop Down 2</a></li>
-                <li><a href="#">Deep Drop Down 3</a></li>
-                <li><a href="#">Deep Drop Down 4</a></li>
-                <li><a href="#">Deep Drop Down 5</a></li>
+                <li>
+                <a href="#">Column 1 link 1</a>
+                <a href="#">Column 1 link 2</a>
+                <a href="#">Column 1 link 3</a>
+                </li>
+                <li>
+                <a href="#">Column 2 link 1</a>
+                <a href="#">Column 2 link 2</a>
+                <a href="#">Column 3 link 3</a>
+                </li>
+                <li>
+                <a href="#">Column 3 link 1</a>
+                <a href="#">Column 3 link 2</a>
+                <a href="#">Column 3 link 3</a>
+                </li>
+                <li>
+                <a href="#">Column 4 link 1</a>
+                <a href="#">Column 4 link 2</a>
+                <a href="#">Column 4 link 3</a>
+                </li>
             </ul>
             </li>
-            <li><a href="#">Drop Down 2</a></li>
-            <li><a href="#">Drop Down 3</a></li>
-            <li><a href="#">Drop Down 4</a></li>
+            <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+                <li><a href="#">Drop Down 1</a></li>
+                <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                <ul>
+                    <li><a href="#">Deep Drop Down 1</a></li>
+                    <li><a href="#">Deep Drop Down 2</a></li>
+                    <li><a href="#">Deep Drop Down 3</a></li>
+                    <li><a href="#">Deep Drop Down 4</a></li>
+                    <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+                </li>
+                <li><a href="#">Drop Down 2</a></li>
+                <li><a href="#">Drop Down 3</a></li>
+                <li><a href="#">Drop Down 4</a></li>
+            </ul>
+            </li>
+            <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li> --}}
+
         </ul>
-        </li>
-        <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li>
-    </ul>
-    <i class="bi bi-list mobile-nav-toggle d-none"></i>
-    </nav><!-- .navbar -->
+        <i class="bi bi-list mobile-nav-toggle d-none"></i>
+        </nav><!-- .navbar -->
 
-    <a class="btn-getstarted scrollto" href="index.html#about">Get Started</a>
+        <div class="btn-getstarted scrollto">
+            <img src="{{ URL::asset('assets/frontend/')}}/img/favicon.png">
+        </div>
+        {{-- <a class="btn-getstarted scrollto"  href="index.html#about">Get Started</a> --}}
 
-</div>
+    </div>
 </header><!-- End Header -->
